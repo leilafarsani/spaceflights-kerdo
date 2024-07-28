617e5bdebd8422cea3d08eb4ca617cab7fb71b38
Leila Y-Farsani <108967493+leilafarsani@users.noreply.github.com>
2024-07-28 17:34:14 +0100

    moving functions into dataprocessing pipeline

--- spaceflight/src/spaceflight/pipelines/dataprocessing/nodes.py
+++ spaceflight/src/spaceflight/pipelines/dataprocessing/nodes.py
@@ -2,3 +2,14 @@
 This is a boilerplate pipeline 'dataprocessing'
 generated using Kedro 0.19.6
 """
+
+import pandas as pd
+
+def _is_true(x: pd.Series) -> pd.Series:
+    return x == "t"
+
+def _parse_percentage(x: pd.Series)-> pd.Series:
+      return x.str.replace('%','').astype(float)
+  
+def _parse_money(x: pd.Series)-> pd.Series:
+      return x.str.replace('$','').str.replace(',','').astype(float)
